cf0e08ec645e919b637fe86f577a625716ec3a95
lxy <neulxy@gmail.com>
2019-06-27 11:37:43 +1200

    ddd

--- SAU/Program.cs
+++ SAU/Program.cs
@@ -9,6 +9,7 @@ namespace SAU
             Console.WriteLine("Hello World!");
             Console.WriteLine("haha");
             string test;
+            int stuid = 5;
             test = Console.ReadLine();
 
         }
